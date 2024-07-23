0d191cbb6e2919834af5d72f6a8e068ffa619422
Gilberto Ribeiro Pinto Júnior <gilbertoribeirocvt@gmail.com>
2024-07-22 21:33:22 -0300

    Initial commit

---
+++ corretor_eq220.py
@@ -0,0 +1,41 @@
+# -------------------- Bibliotecas --------------------
+
+
+import re
+import os
+import argparse
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# -------------------- Constantes --------------------
+
+
+# -------------------- Classes --------------------
+
+
+class Atividade:
+    pass
+
+
+class Exercicio:
+    pass
+
+
+class Aluno:
+    pass
+
+
+# -------------------- Funções --------------------
+
+
+# -------------------- Função main() --------------------
+
+
+def main() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    main()
